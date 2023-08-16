0d17adccab9ce92e4a0b7523485db3948eb54601
rockstargett@gmail.com <rockstargett@gmail.com>
2023-08-16 19:00:19 +0300

    initial commit 9

--- medusa-config.js
+++ medusa-config.js
@@ -27,6 +27,7 @@ const ADMIN_CORS =
 
 // CORS to avoid issues when consuming Medusa from a client
 const STORE_CORS = process.env.STORE_CORS || "http://localhost:8000";
+const STORE_CORS_NEW = process.env.STORE_CORS || "https://ttstorefr.vercel.app/";
 
 const DATABASE_URL =
   process.env.DATABASE_URL || "postgres://localhost/medusa-store";
